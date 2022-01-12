56537479989e5f930e246ec847d5e25cfcf4ffea
GitHub Action <action@github.com>
2022-01-12 14:14:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/gretzki/swiftalyzerplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gretzki/swiftalyzerplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gretzki.swiftalyzerplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gretzki.swiftalyzerplugin.MyBundle
 
 class MyApplicationService {
 
